310e20df04ac0a997dae032a40e4c347e663b220
Amey Rajesh Shaligram <amey-rajesh.shaligram@s2023.tu-chemnitz.de>
2024-12-17 17:19:02 +0100

    Add react router

--- 5iveBit-app/src/renderer/src/components/App/App.jsx
+++ 5iveBit-app/src/renderer/src/components/App/App.jsx
@@ -1,22 +1,42 @@
 import { Box, Stack } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChatsProvider } from '../../contexts/ChatContext';
 import styles from './App.module.css';
 import ChatBox from '../chat/ChatBox';
 import NavBar from '../common/NavBar';
 import RightBar from '../common/RightBar';
+// import Login from '../auth/Login';  // Commented out for now
+// import SignUp from '../auth/SignUp';  // Commented out for now
+// import PrivateRoute from '../common/PrivateRoute'; // PrivateRoute for secured routes (commented out for now)
 
 function App() {
   return (
     <ChatsProvider>
-      <Box className={styles.container}>
-        <NavBar />
-        <Box className={styles.content}>
-          <Stack direction="row" className={styles.stack}>
-            <ChatBox />
-            <RightBar />
-          </Stack>
+      <Router>
+        <Box className={styles.container}>
+          <Routes>
+            {/* Define routes for login and signup */}
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/signup" element={<SignUp />} /> */}
+
+            {/* Main app content route */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <NavBar />
+                  <Box className={styles.content}>
+                    <Stack direction="row" className={styles.stack}>
+                      <ChatBox />
+                      <RightBar />
+                    </Stack>
+                  </Box>
+                </>
+              }
+            />
+          </Routes>
         </Box>
-      </Box>
+      </Router>
     </ChatsProvider>
   );
 }
