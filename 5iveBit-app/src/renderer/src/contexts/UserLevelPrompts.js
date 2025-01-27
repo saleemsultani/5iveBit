9303ac7ac295c91fc659cdd7d177a0689260ca77
Ashlyn <ashlyn.higgins@s2023.tu-chemnitz.de>
2025-01-27 23:54:29 +0100

    Added user experience level prompts

---
+++ 5iveBit-app/src/renderer/src/contexts/UserLevelPrompts.js
@@ -0,0 +1,12 @@
+export const beginnerPrompt = 
+'STRICTLY explain cybersecurity concepts in simple, approachable terms, assuming the audience has a solid understanding of computer science but limited knowledge of cybersecurity. Use basic technical terminology when necessary but prioritize clarity. Break down topics step by step, connecting them to familiar computer science concepts like networks, databases, and programming. Provide real-world examples to make cybersecurity ideas and concepts relatable.';
+
+export const intermediatePrompt = 
+'STRICTLY explain cybersecurity concepts using technical terminology appropriate for someone with a computer science background and intermediate knowledge of cybersecurity. Provide detailed explanations of advanced cybersecurity concepts and include practical examples where applicable. Include implementation specific details and information.';
+
+
+export const expertPrompt = 
+'STRICTLY explain cybersecurity concepts with a focus on advanced technical depth. Assume the user has a computer science background and extensive knowledge of cybersecurity.  Use industry standard terminology and provide in-depth explanations of cybersecurity concepts and technology. Include implementation details, theoretical considerations, and practical examples to convey a comprehensive understanding.';
+
+
+
